a346454985ad58dffc19112af175821b635513d1
Shikhkerimov Shikhkerim <kerim_shikhkerimov@mail.ru>
2024-05-04 20:12:16 +0400

    edited slider (portfolio section, furniture order page)

--- src/js/portfolio-slider.js
+++ src/js/portfolio-slider.js
@@ -1,9 +1,9 @@
 document.getElementById('next').onclick = function() {
-    let lists = document.querySelectorAll('.item');
+    let lists = document.querySelectorAll('.slide__img');
     document.getElementById('slide').appendChild(lists[0]);
 }
 
 document.getElementById('prev').onclick = function() {
-    let lists = document.querySelectorAll('.item');
+    let lists = document.querySelectorAll('.slide__img');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
